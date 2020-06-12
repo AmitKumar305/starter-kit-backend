fcdb08bbfc33d6b7c99263f176ee3cb1f519acec
Santgurlal singh <gurlal050@gmail.com>
2020-06-13 01:15:14 +0530

    removing mongoDb deprecation warnings  (#15)
    * removing ensure index is deprecated warning
    
    * removing useUnifiedTopology warning
    
    Co-authored-by: Gurlal singh <gurlalsingh@Gurlals-MacBook-Pro.local>

--- server/db/index.js
+++ server/db/index.js
@@ -10,9 +10,12 @@ import { Promise as es6Promise } from 'es6-promise';
 import { mongoConnectionString } from '../constants';
 
 const useNewUrlParser = true;
-
+const useCreateIndex = true;
+const useUnifiedTopology = true;
 mongoose.Promise = es6Promise;
-mongoose.connect(mongoConnectionString, { useNewUrlParser }, (err) => {
+mongoose.connect(mongoConnectionString, {
+	useNewUrlParser, useCreateIndex, useUnifiedTopology,
+}, (err) => {
 	if (err) {
 		console.log('mongo connection err', err);
 	} else {
