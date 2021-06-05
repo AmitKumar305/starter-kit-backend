843a250a6b46d25f7a23bc6aed83acbcacc5cf3b
Sahil811 <sahilsiddiqui.dev@gmail.com>
2021-06-05 13:55:31 +0530

    ASK-4: userlist api update

--- server/model/admin/userList.js
+++ server/model/admin/userList.js
@@ -1,6 +1,7 @@
 import { ResponseUtility } from 'appknit-backend-bundle';
 import { UserModel } from '../../schemas';
 import { PAGINATION_LIMIT } from '../../constants';
+import { delete } from 'request-promise';
 
 /**
 * @description service model function to fetch the listing of the users
@@ -14,14 +15,20 @@ export default ({
 	limit = PAGINATION_LIMIT,
 }) => new Promise(async (resolve, reject) => {
 	try {
+		const andQuery = [
+			{ deleted: false }
+		];
+
+		if(text) {
+			andQuery.push(
+				{ email: new RegExp(text, 'i') },
+				{ name: new RegExp(text, 'i') },
+			)
+		};
+		
 		const [data] = await UserModel.aggregate([
 			{
-				$match: text ? {
-					$or: [
-						{ email: new RegExp(text, 'i') },
-						{ name: new RegExp(text, 'i') },
-					],
-				} : { },
+				$and: andQuery,
 			},
 			{
 				$unset: [
